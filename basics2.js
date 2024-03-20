bad4cd9fcd94dd6d560a205076c4b46320d96f36
Satvik Bhanwal <satvikbhanwal5678@gmail.com>
2024-03-21 01:25:37 +0530

    adding typeof file

---
+++ basics2.js
@@ -0,0 +1,11 @@
+//typeof
+let sum = 5;
+console.log(typeof sum);
+let uname = "satvik";
+console.log(typeof uname);
+let obj = {};
+console.log(typeof obj);
+let unde = undefined;
+console.log(typeof unde);
+let n = null;
+console.log(typeof n);
