4a8a6f3f35c6015ee4eaee497c604e4d3cc36b7e
Satvikbhanwal <60311891+satvik5678@users.noreply.github.com>
2024-03-21 07:48:40 +0000

    "control-flow code added"

---
+++ basics/control-flow.js
@@ -0,0 +1,18 @@
+let num=5;
+if(num>1)
+{
+    console.log("greater than one");
+}
+else
+{
+    console.log("lesser than one");
+}
+
+/*
+    > , < , <= , >= , == (only check the value) , != , ===(check value and type both)
+*/
+
+//nullish coalescing operator (??)
+var1 = null ?? undefined
+
+console.log(var1);
